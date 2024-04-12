4ea916f349909140037c41935ffa0e894bce4aa5
Ilya Gusev <phoenixilya@gmail.com>
2024-04-12 19:15:53 +0000

    fix

--- run_temp150.sh
+++ run_temp150.sh
@@ -3,3 +3,5 @@ python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_minp_02.js
 python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_minp_05.jsonl "openchat/openchat-3.5-0106" configs/temp150_minp_05.json
 
 python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_minp_10.jsonl "openchat/openchat-3.5-0106" configs/temp150_minp_10.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_minp_20.jsonl "openchat/openchat-3.5-0106" configs/temp150_minp_20.json
