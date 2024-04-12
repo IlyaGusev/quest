b75570c953bb450d3cb1d8c840510b1b5a5e468e
Ilya Gusev <phoenixilya@gmail.com>
2024-04-12 11:43:00 +0000

    sh fix

--- run_dynatemp.sh
+++ run_dynatemp.sh
@@ -2,4 +2,6 @@ python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_dynatemp_50_150_10
 
 python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_dynatemp_50_150_75_minp_05.jsonl "openchat/openchat-3.5-0106" configs/dynatemp_50_150_75_minp_05.json
 
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_dynatemp_100_250_100_minp_10.jsonl "openchat/openchat-3.5-0106" configs/dynatemp_100_250_100_minp_10.json
 
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_dynatemp_50_300_100_minp_10.jsonl "openchat/openchat-3.5-0106" configs/dynatemp_50_300_100_minp_10.json
