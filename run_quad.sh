2ef439bceee495e09a4fa2f39945ae037a06a5b4
Ilya Gusev <phoenixilya@gmail.com>
2024-04-13 21:57:40 +0000

    quad_15

---
+++ run_quad.sh
@@ -0,0 +1,5 @@
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_quad_25_100.jsonl "openchat/openchat-3.5-0106" configs/quad_25_100.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_quad_20_100.jsonl "openchat/openchat-3.5-0106" configs/quad_20_100.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_quad_15_100.jsonl "openchat/openchat-3.5-0106" configs/quad_15_100.json
