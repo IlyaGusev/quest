8e65dfae23bd65f6078064b3695be7254a48f53f
Ilya Gusev <phoenixilya@gmail.com>
2024-04-13 08:40:37 +0000

    sh

---
+++ run_tfs.sh
@@ -0,0 +1,5 @@
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_tfs_95.jsonl "openchat/openchat-3.5-0106" configs/tfs_95.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_tfs_98.jsonl "openchat/openchat-3.5-0106" configs/tfs_98.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_tfs_95.jsonl "openchat/openchat-3.5-0106" configs/temp150_tfs_95.json
