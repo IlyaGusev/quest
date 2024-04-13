fff8a04f731b11c890672e3caf835289a4655383
Ilya Gusev <phoenixilya@gmail.com>
2024-04-13 17:12:11 +0000

    Updates

--- run_minp.sh
+++ run_minp.sh
@@ -3,3 +3,5 @@ python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp170_minp_15.js
 python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp200_minp_20.jsonl "openchat/openchat-3.5-0106" configs/temp200_minp_20.json
 
 python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp150_minp_15.jsonl "openchat/openchat-3.5-0106" configs/temp150_minp_15.json
+
+python3 -m quest.infer data/prompts/all.jsonl data/outputs/7b_temp120_minp_10.jsonl "openchat/openchat-3.5-0106" configs/temp120_minp_10.json
